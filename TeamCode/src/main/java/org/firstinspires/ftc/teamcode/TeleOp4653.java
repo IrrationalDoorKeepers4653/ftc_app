4c7a2b58c49df3a010d3f80462f2977ba926e1e1
Smlilley1999 <smlilley1999@gmail.com>
2018-01-26 23:10:41 -0500

    Fixed problem with phones not initializing
    I took away the public class TeleOp4653 that was above @TeleOp() and renamed public class BasicOpMode_Iterative to public class TeleOp4653. This solved teh problem with the phones not initializing.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp4653.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp4653.java
@@ -10,7 +10,6 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Created by silkenhons on 10/17/2017.
  */
 
-public class TeleOp4653 {
     /* Copyright (c) 2017 FIRST. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
@@ -22,10 +21,11 @@ public class TeleOp4653 {
  *
 */
 
-    @TeleOp(name="Basic: TeleOp4653", group="Iterative Opmode")
+    @TeleOp(name="Basic: BasicOpMode_Iterative", group="Iterative Opmode")
 
-    public class BasicOpMode_Iterative extends OpMode
-    {
+    public class TeleOp4653 extends OpMode {
+
+        HardwarePushbot robot     = new HardwarePushbot();
         // Declare OpMode members.
         private ElapsedTime runtime = new ElapsedTime();
         private DcMotor leftDrive = null;
@@ -48,6 +48,7 @@ public class TeleOp4653 {
          */
         @Override
         public void init() {
+
             telemetry.addData("Status", "Initialized");
 
             // Initialize the hardware variables. Note that the strings used here as parameters
@@ -77,6 +78,7 @@ public class TeleOp4653 {
          */
         @Override
         public void init_loop() {
+            telemetry.addLine("Hi! Initialization Complete!");
         }
 
         /*
@@ -142,4 +144,3 @@ public class TeleOp4653 {
 
     }
 
-}
